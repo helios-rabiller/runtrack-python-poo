3fdb2265c48fd5ef8abad88e2c629703334b7ec5
Helios-rabiller-LP <helios.rabiller@laplateforme.io>
2025-02-04 13:51:31 +0100

    feat : new job 5 added

--- jour02/job05.py
+++ jour02/job05.py
@@ -1,5 +1,5 @@
 class Voiture:
-    def __init__(self,marque,modele,an,kilometrage,reservoir= 50,en_marche= False):
+    def __init__(self,marque,modele,an,kilometrage,reservoir= 5,en_marche= False):
         self.__marque = marque
         self.__modele = modele
         self.__an = an
@@ -7,7 +7,7 @@ class Voiture:
         self.__en_marche = en_marche
         self.__reservoir = reservoir
 
-    def get_reservoir(self):
+    def __verifier_plein__(self):
         return self.__reservoir
     def get_marque(self):
         return self.__marque
@@ -19,9 +19,28 @@ class Voiture:
         return self.__kilometrage
     def get_en_marche(self):
         return self.__en_marche
+    def set_marque(self,new_marque):
+        self.__marque = new_marque
+    def set_modele(self,new_modele):
+        self.__modele = new_modele
+    def set_an(self,new_an):
+        self.__an = new_an
+    def set_kilometrage(self,new_kilometrage):
+        self.__kilometrage = new_kilometrage
+    def set_en_marche(self,new_en_marche):
+        self.__en_marche = new_en_marche
+    def set_reservoir(self,new_reservoir):
+        self.__reservoir = new_reservoir
     def demarrer(self):
-        self.__en_marche=True
-        return self.__en_marche
+        if self.__verifier_plein__() > 5:
+            self.__en_marche=True
+            print("demarre")
+        else:
+            self.arreter()
     def arreter(self):
         self.__en_marche=False
-        return self.__en_marche
+        print("arrêt")
+
+voiture = Voiture("Citroen","C4 Picasso",4,134900)
+
+voiture.demarrer()
